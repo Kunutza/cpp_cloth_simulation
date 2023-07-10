00d27cd4d4ffece1fbcd017a9e8fd199cbb63245
kunutza <4lexkott@gmail.com>
2023-07-10 10:21:02 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
+#include "src/physics/particle.hpp"
+
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Cloth-Project", sf::Style::Default);
+    const uint32_t window_width = 800;
+    const uint32_t window_height = 600;
+
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Cloth-Project", sf::Style::Default);
     window.setFramerateLimit(60);
 
     while (window.isOpen()) {
