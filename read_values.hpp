c0f64d6af7fb584a69b3151e813bb4ccef509380
kunutza <4lexkott@gmail.com>
2023-07-14 23:19:14 +0300

    may change its location

--- read_values.hpp
+++ read_values.hpp
@@ -18,70 +18,12 @@ struct Reader {
         return str;
     }
 
-    static const string get_window_width(const string stringfile) {
-        regex rgx("window_width=(\\w+)");
+    static const string get_value(const string stringfile, const string value_name) {
+        regex rgx(value_name + "=(\\w+)");
         smatch match;
 
         if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
             return match[1];
-        return default_values_map.find("window_width")->second;
-    }
-
-
-    static const string get_window_height(const string stringfile) {
-        regex rgx("window_height=(\\w+)");
-        smatch match;
-
-        if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
-            return match[1];
-        return default_values_map.find("window_height")->second;
-    }
-
-
-    static const string get_frame_rate(const string stringfile) {
-        regex rgx("frame_rate=(\\w+)");
-        smatch match;
-
-        if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
-            return match[1];
-        return default_values_map.find("frame_rate")->second;
-    }
-
-    static const string get_time_multitplier(const string stringfile) {
-        regex rgx("time_multiplier=(\\w+)");
-        smatch match;
-
-        if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
-            return match[1];
-        return default_values_map.find("time_multiplier")->second;
-    }
-
-    static const string get_cloth_width(const string stringfile) {
-        regex rgx("cloth_width=(\\w+)");
-        smatch match;
-
-        if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
-            return match[1];
-        return default_values_map.find("cloth_width")->second;
-    }
-
-
-    static const string get_cloth_height(const string stringfile) {
-        regex rgx("cloth_height=(\\w+)");
-        smatch match;
-
-        if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
-            return match[1];
-        return default_values_map.find("cloth_height")->second;
-    }
-
-
-    static const string get_links_lenght(const string stringfile) {
-        regex rgx("links_length=(\\w+)");
-        smatch match;
-
-        if (regex_search(stringfile.begin(), stringfile.end(), match, rgx))
-            return match[1];
-        return default_values_map.find("links_length")->second;
+        return default_values_map.find(value_name)->second; 
     }
 };
